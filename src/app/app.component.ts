40d7f86f0a768e8108d14dd05979816f96c921df
Kara Erickson <karakara@google.com>
2016-08-03 14:40:24 -0700

    add code from webinar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
 import {NewsletterComponent} from "./newsletter/newsletter.component";
-import {FormGroup, FormControl, FormBuilder, Validators, FormArray} from "@angular/forms";
+import {FormGroup, FormControl, FormBuilder, Validators, FormArray, REACTIVE_FORM_DIRECTIVES} from "@angular/forms";
 
 @Component({
   moduleId: module.id,
@@ -14,10 +14,43 @@ import {FormGroup, FormControl, FormBuilder, Validators, FormArray} from "@angul
     MD_CARD_DIRECTIVES,
     MD_BUTTON_DIRECTIVES,
     MD_INPUT_DIRECTIVES,
-    NewsletterComponent
+    NewsletterComponent,
+    REACTIVE_FORM_DIRECTIVES
   ],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+  form: FormGroup;
+  addresses: FormArray;
 
+  constructor(private _fb: FormBuilder) {
+    this.form = this._fb.group({
+      first: 'Nancy',
+      last: '',
+      username: '',
+      addresses: this.buildArray(),
+      newsletter: '',
+      robot: ''
+    });
+  }
+
+  buildArray(): FormArray {
+    this.addresses = this._fb.array([
+      this.buildGroup()
+    ]);
+    return this.addresses;
+  }
+
+  buildGroup(): FormGroup {
+    return this._fb.group({
+      street: '',
+      city: '',
+      state: 'NY',
+      zip: '',
+    });
+  }
+
+  add() {
+    this.addresses.push(this.buildGroup());
+  }
 }
